a852a1021992b91761096b00becab03f425a4d03
LM1414 <lmariat@etu.isara.fr>
2021-05-03 16:12:12 +0200

    maj serveur

--- Applileatheo/server.R
+++ Applileatheo/server.R
@@ -1,26 +1,23 @@
-#
-# This is the server logic of a Shiny web application. You can run the
-# application by clicking 'Run App' above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
-library(shiny)
-
-# Define server logic required to draw a histogram
-shinyServer(function(input, output) {
-
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
-    })
-
-})
+server <- function(input, output, session) {
+  
+  plot2 <- reactive(if("All" %in% input$valuechain){
+    food_new
+  }else{food_new %>% filter(fsc_location1 %in% input$valuechain)
+  }
+  )
+  
+  plot1 <- reactive(plot2() %>% filter(year >= input$year[1] & year <= input$year[2]))
+  
+  output$box_plot <- renderPlotly(
+    ggplotly(plot1() %>% ggplot(aes(fsc_location1,loss_per_clean,
+                                    col=fsc_location1,fill=fsc_location1)) +
+               geom_boxplot(alpha=0.5) +
+               xlab("") + 
+               ylab("Percentage loss (%)") +
+               theme(axis.text.x=element_text(angle = 45)) +
+               theme(legend.title = element_blank()) +
+               labs(caption = "Data source: http://www.fao.org/platform-food-loss-waste/flw-data/en/"))
+  )
+  
+  output$fooddata <- renderDataTable(food_new, options = list(pageLength = 10))
+}
